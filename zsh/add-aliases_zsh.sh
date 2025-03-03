7b4bc3ff52039a6c42da4b0c40bf1894659db580
Hudson <hudson@thux.tech>
2025-03-03 13:54:24 -0300

    new aliases

--- zsh/add-aliases_zsh.sh
+++ zsh/add-aliases_zsh.sh
@@ -1,4 +1,2 @@
 # add the below lines to the zshrc file so that the aliases file can be recognized
-if [ -f ~/.zsh_aliases ]; then
-    . ~/.zsh_aliases
-fi
+echo "[ -f ~/.zsh_aliases ] &&. ~/.zsh_aliases" >> ~/.zshrc
